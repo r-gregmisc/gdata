4182b2da7d1491141b326cf5b921cf2053672784
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2015-06-29 22:30:00 +0000

    Add 'byrow' argument to lowerTriangle()/upperTriangle() functions.

--- pkg/gdata/R/upperTriangle.R
+++ pkg/gdata/R/upperTriangle.R
@@ -1,22 +1,42 @@
-upperTriangle <- function(x, diag=FALSE)
+upperTriangle <- function(x, diag=FALSE, byrow=FALSE)
   {
-    x[upper.tri(x, diag=diag)]
+    if(byrow)
+      t(x)[rev(upper.tri(x, diag=diag))]
+    else
+      x[upper.tri(x, diag=diag)]
   }
 
-"upperTriangle<-" <- function(x, diag=FALSE, value)
+"upperTriangle<-" <- function(x, diag=FALSE, byrow=FALSE, value)
   {
-    x[upper.tri(x, diag=diag)] <- value
-    x
+    if(byrow) {
+      ret <- t(x)
+      ret[rev(upper.tri(x, diag=diag))] <- value
+      t(ret)
+    }
+    else {        
+      x[upper.tri(x, diag=diag)] <- value
+      x
+    }
   }
 
-lowerTriangle <- function(x, diag=FALSE)
+lowerTriangle <- function(x, diag=FALSE, byrow=FALSE)
   {
+  if(byrow)
+    t(x)[rev(lower.tri(x, diag=diag))]
+  else
     x[lower.tri(x, diag=diag)]
   }
 
-"lowerTriangle<-" <- function(x, diag=FALSE, value)
+"lowerTriangle<-" <- function(x, diag=FALSE, byrow=FALSE, value)
   {
+  if(byrow) {
+    ret <- t(x)
+    ret[rev(lower.tri(x, diag=diag))] <- value
+    t(ret)
+  }
+  else {        
     x[lower.tri(x, diag=diag)] <- value
     x
   }
+}
 
