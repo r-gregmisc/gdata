d056564bfb8454eccc111e087d5e04425bc46cf3
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2016-02-03 20:15:02 +0000

    Add update() methods for lists and data frames

---
+++ R/update.list.R
@@ -0,0 +1,20 @@
+## this function updates the elements of list 'object' to contain all of the elements
+## of 'new', overwriting elements with the same name, and (optionally) copying unnamed
+## elements.
+update.list <- function(object, new, unnamed=FALSE, ...)
+{
+  retval <- object
+
+  for(name in names(new))
+    retval[[name]] <- new[[name]]
+
+  if(unnamed)
+  {
+    if(is.null(names(new)))
+      names(new) <- rep("", length=length(new))
+    for(i in (1:length(new))[names(new)==""] )
+        retval <- append(retval, new[[i]])
+  }
+
+  retval
+}
