b91aa0b58ea480052367d801468c99b501d106e1
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2016-02-03 20:13:49 +0000

    Add assignment versions of first() and last()

--- R/first.R
+++ R/first.R
@@ -1,3 +1,16 @@
 # Simply call 'first' or 'last' with a different default value for 'n'.
 first <- function(x, n=1, ...) head(x, n=n, ...)
 last  <- function(x, n=1, ...) tail(x, n=n, ...)
+
+"first<-" <- function(x, n=1, ..., value )
+{
+  x[1:n] <- value[1:n]
+  x
+}
+
+"last<-" <- function(x, n=1, ..., value )
+{
+  index <- seq( length(x)-n+1, length(x) )
+  x[index] <- value[1:n]
+  x
+}
