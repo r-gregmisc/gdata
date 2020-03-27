aa6551e2bc0d9204083817cb45dea1c637a505d9
Arni Magnusson <arni.magnusson@ices.dk>
2020-03-27 18:10:27 +0100

    Sync with 2.18.0 on CRAN

--- R/update.data.frame.R
+++ R/update.data.frame.R
@@ -1,33 +1,48 @@
-# This function replace rows in 'x' by corresponding rows in 'y' the have 
-# the same value for 'by'
-update.data.frame <- function(x, y, by, by.x=by, by.y=by, append=TRUE, verbose=TRUE, ...)
-{
-  retval <- x
-  x.by <- x[[by.x]]
-  y.by <- y[[by.y]]
-  
-  matches.x <- match(y.by, x.by)
-  matches.y <- which(!is.na(matches.x))
-  nomatch.y <- which(is.na(matches.x))
-  matches.x <- matches.x[!is.na(matches.x)]
-  
-  if(length(matches.x)>0)
-    retval[matches.x, ] <- y[matches.y,]
-  
-  if(length(nomatch.y) && append)  
-    retval <- rbind(retval, y[nomatch.y,])
-  
-  if(verbose)
-  {
-    cat("\n")    
-    cat("Number of rows in x     :", nrow(x),           "\n")
-    cat("Number of rows in y     :", nrow(y),           "\n")
-    cat("\n")
-    cat("Number of rows replaced :", length(matches.x), "\n")
-    cat("Number of rows appended :", length(nomatch.y), "\n")
-    cat("\n")
-    cat("Number of rows in result:", nrow(retval),      "\n")
-    cat("\n")
-  }
-  retval
-}
+# # This function replace rows in 'object' by corresponding rows in 'new' that have
+# # the same value for 'by'
+# update.data.frame <- function(object,
+#                               new,
+#                               by,
+#                               by.object=by,
+#                               by.new=by,
+#                               append=TRUE,
+#                               verbose=TRUE,
+#                               ...)
+# {
+#   retval <- object
+#
+#   if(length(by.object)>1)
+#     stop("'by.object' can specify at most one column")
+#
+#   if(length(by.new)>1)
+#     stop("'by.new' can specify at most one column")
+#
+#
+#   object.by <- object[[by.object]]
+#   new.by    <- new   [[by.new   ]]
+#
+#   matches.object <- match(new.by, object.by)
+#   matches.new    <- which(!is.na(matches.object))
+#   nomatch.new    <- which(is.na(matches.object))
+#   matches.object <- matches.object[!is.na(matches.object)]
+#
+#   if(length(matches.object)>0)
+#     retval[matches.object, ] <- new[matches.new,]
+#
+#   if(length(nomatch.new) && append)
+#     retval <- rbind(retval, new[nomatch.new,])
+#
+#   if(verbose)
+#   {
+#     cat("\n")
+#     cat("Number of rows in object:", nrow(object),           "\n")
+#     cat("Number of rows in new   :", nrow(new),           "\n")
+#     cat("\n")
+#     cat("Number of rows replaced :", length(matches.object), "\n")
+#     cat("Number of rows appended :", length(nomatch.new), "\n")
+#     cat("\n")
+#     cat("Number of rows in result:", nrow(retval),      "\n")
+#     cat("\n")
+#   }
+#   retval
+# }
